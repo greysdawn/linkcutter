1e07a65698090ca8a0f47bbd42068b926bda4d60
xgreyskiesx <greysdawn@gmail.com>
2019-04-21 18:27:32 -0400

    Resolving Heroku issues

--- index.js
+++ index.js
@@ -91,13 +91,11 @@ app.get("/",async (req,res)=>{
 		var user = JSON.parse(req.cookies.user);
 		var valid = await isValidUser(user.name, user.pass);
 		if(!valid) {
-			console.log(req.cookies.user);
 			res.sendFile(path.resolve("pages/login.html"));
 		} else {
 			res.sendFile(path.resolve("pages/index.html"));
 		}
 	} else {
-		console.log(req.cookies.user);
 		res.sendFile(path.resolve("pages/login.html"));
 	}
 })
